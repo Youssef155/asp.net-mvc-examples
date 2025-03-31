fd95788c8b4439627786f16e36ce176bd90f2ce3
Youssef George <yousifkhaleel00@gmail.com>
2025-03-31 13:10:55 +0200

    partial view example

---
+++ PartialViewExample/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PartialViewExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewData["ListTitle"] = "Countries";
+            ViewData["ListItems"] = new List<string>()
+            {
+                "Egypt",
+                "UK",
+                "USA",
+                "France",
+                "Japan"
+            };
+
+
+            return View();
+        }
+
+        [Route("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
